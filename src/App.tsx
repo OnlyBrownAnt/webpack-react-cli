f9463cce2610ae28b25286e41ce6395d6144d017
zhangyao <45zhangye@gmail.com>
2024-01-01 23:00:28 +0800

    feat: add css modules setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react'
-import './index.less'
+// import './index.less'
+import indexStyle1 from './index1.module.less';
+import indexStyle2 from './index2.module.less';
+
 const testPng = require("./assets/imgs/arithmetic-1.png");
 const App: React.FC = () => {
   return <>
-    <div className="wrapper">
+    <div className={indexStyle1.wrapper}>
       hello {process.env.REACT_APP_PUBLIC_URL}
-      <img src={testPng}></img>
+      <img src={testPng} className={indexStyle2.wrapper}></img>
     </div>
   </>
 }
